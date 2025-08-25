01a0d3db3fed5fbc9b63ea292e9fefbc75950753
Tom Keddie <thomaskeddie@gmail.com>
2025-08-25 18:23:02 +0000

    can the ime slots in ar darts bookings be 12 hour format not 24hr please

--- src/components/booking-form.tsx
+++ src/components/booking-form.tsx
@@ -18,7 +18,7 @@ import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
 
 const timeSlots = [
-    "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00"
+    "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM"
 ];
 
 const formSchema = z.object({
