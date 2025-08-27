43235ff8ecb1ff3116a434a2c43215c0faf285a3
Tom Keddie <thomaskeddie@gmail.com>
2025-08-27 09:09:08 +0000

    ok no probelm.  well leave it as is.  One other thing.  Ive noticed the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
